ce8ec6b11eeeba7bd6b520e71bef0313c4f477d6
Ravi Desai <ravi@ravidesai.com>
2019-02-03 14:53:49 -0500

    Test basic rover movement
    This change allows rovers to respond to commands.

--- run_rovers.rb
+++ run_rovers.rb
@@ -1,8 +1,10 @@
 #!/usr/local/bin/ruby
 
-input = File.read("./input.txt")
+def print_input(input = "./input.txt")
+  input = File.read(input)
 
-p input
+  p input
+end
 
 def plateau_size(input)
   input.split('\n').first
@@ -19,5 +21,69 @@ class Rover
 
   def to_s
     "#{x} #{y} #{direction}"
+  end
+
+  def parse_next_command(command)
+    case command
+    when "M"
+      move_forward
+    when "L"
+      rotate_left
+    when "R"
+      rotate_right
+    else
+      raise ArgumentError, "#{command} is an invalid command"
+    end
+  end
+
+  def rotate_right
+    @direction = case @direction
+                 when 'N'
+                   'E'
+                 when 'E'
+                   'S'
+                 when 'S'
+                   'W'
+                 when 'W'
+                   'N'
+                 else
+                   raise "Direction should not be #{direction}."
+                 end
+
+    self
+  end
+
+  def rotate_left
+    @direction = case @direction
+                 when 'N'
+                   'W'
+                 when 'E'
+                   'N'
+                 when 'S'
+                   'E'
+                 when 'W'
+                   'S'
+                 else
+                   raise "Direction should not be #{direction}."
+                 end
+
+    self
+  end
+
+  def move_forward
+    case @direction
+    when 'N'
+      @y += 1
+    when 'E'
+      @x += 1
+    when 'S'
+      @y -= 1
+    when 'W'
+      @x -= 1
+    else
+      raise "Direction should not be #{direction}."
+    end
+
+    self
   end
 end
--- test/test_run_rovers.rb
+++ test/test_run_rovers.rb
@@ -2,7 +2,32 @@ require "minitest/autorun"
 load "run_rovers.rb"
 
 class RoverTest < Minitest::Test
-  def test_print
-    assert_equal "2 3 W", Rover.new(2, 3, 'W').to_s
+  def setup
+    @rover = Rover.new(2, 3, 'W')
+  end
+
+  def test_to_s
+    assert_equal "2 3 W", @rover.to_s
+  end
+
+  def test_rotate_right
+    assert_equal 'N', @rover.rotate_right.direction
+    assert_equal 'E', @rover.rotate_right.direction
+    assert_equal 'S', @rover.rotate_right.direction
+    assert_equal 'W', @rover.rotate_right.direction
+  end
+
+  def test_rotate_left
+    assert_equal 'S', @rover.rotate_left.direction
+    assert_equal 'E', @rover.rotate_left.direction
+    assert_equal 'N', @rover.rotate_left.direction
+    assert_equal 'W', @rover.rotate_left.direction
+  end
+
+  def test_move_forward
+    assert_equal '2 4 N', @rover.rotate_right.move_forward.to_s
+    assert_equal '3 4 E', @rover.rotate_right.move_forward.to_s
+    assert_equal '3 3 S', @rover.rotate_right.move_forward.to_s
+    assert_equal '2 3 W', @rover.rotate_right.move_forward.to_s
   end
 end
